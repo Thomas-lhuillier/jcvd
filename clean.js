9764387e4226076d2ec85b49fa4683fe48f5d40f
Guillaume Wagner <Guillaume Wagner>
2017-02-13 22:54:19 +0100

    Save JSON

--- clean.js
+++ clean.js
@@ -1,5 +1,7 @@
 const log = require( "./log.json" )
 const moment = require('moment');
+const fs = require('fs');
+gi
 const nbjours = 30;
 let time = Math.floor(Date.now() / 1000);
 console.log('Time: ' + time)
@@ -10,10 +12,21 @@ function clean(){
             if ( delta > (nbjours * 86400)){
                 delete log[key];
                 nblines++
+                console.log('Supprime: poste il y a ' + moment.duration(delta, "seconds").humanize())
 
             }
             else { console.log('Non supprime: poste il y a ' + moment.duration(delta, "seconds").humanize())}
         }
     console.log('Termine: ' + nblines + ' lignes supprimees')
+    save_json(log)
 }
 clean();
+
+function save_json(text){
+    console.log(text);
+    let json = JSON.stringify(text);
+    fs.writeFile("log.json", json, (err) => {
+        if (err) throw err;
+    console.log('JSON updated');
+});
+}
